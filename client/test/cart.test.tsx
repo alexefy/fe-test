958a69907eb955c6fe94eabfcce58758bb770777
alexefy <alexefthymiou@gmail.com>
2023-02-15 13:51:06 +0000

    hotfix/type-issue-in-test: changed the how classes where being checked in test to fix ts error

--- client/test/cart.test.tsx
+++ client/test/cart.test.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { expect, test } from '@jest/globals'
+import { test } from '@jest/globals'
 import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom'
 import Cart from '../components/cart'
 
 test('should be able to toggle the visibility of cart with click or scroll', async () => {
   const { getByTitle } = render(<Cart total={3} />)
 
   // Cart is hidden by default
-  expect(getByTitle('cart')).toHaveClass('hidden')
+  const cartElement = getByTitle('cart')
+  expect(cartElement.classList.contains('hidden')).toBe(true)
 
   // Click the cart button to show the cart
   fireEvent.click(getByTitle('cart-button'))
-  expect(getByTitle('cart')).not.toHaveClass('hidden')
+
+  // The cart should not have the "hidden" class anymore
+  expect(cartElement.classList.contains('hidden')).toBe(false)
 })
