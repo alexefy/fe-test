1064e2fb725af96f7cf0cff2d86c111a2d25e1a1
alexefy <alexefthymiou@gmail.com>
2023-02-15 10:09:06 +0000

    feature/footer: added layout and container component

---
+++ client/components/container/container.tsx
@@ -0,0 +1,7 @@
+export interface ContainerProps {
+  children: React.ReactNode
+}
+
+const Container = ({children}: ContainerProps) => <div className="max-w-[800px] w-full mx-auto">{children}</div>
+
+export default Container
