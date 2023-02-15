b3edd2efd7e0c0944f99ad9a153e503515670a7b
alexefy <alexefthymiou@gmail.com>
2023-02-15 10:03:11 +0000

    feature/header: refactored scroll into custom hook

---
+++ client/hooks/useScrollHandler.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+export function useScrollHandler() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [lastScrollPos, setLastScrollPos] = useState(0);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    if (currentScrollPos < lastScrollPos && isScrolled) {
+      setIsScrolled(false);
+    } else if (currentScrollPos > lastScrollPos && !isScrolled) {
+      setIsScrolled(true);
+    }
+    setLastScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  return [isScrolled, lastScrollPos];
+}
